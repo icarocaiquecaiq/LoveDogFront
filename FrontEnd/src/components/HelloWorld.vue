3a863f533c25dccb6a5cf2075e162f7a748683a7
icarocaiquecaiq <icarocaiquevp16@gmail.com>
2024-10-24 16:51:03 -0300

    firstSetup

---
+++ FrontEnd/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="p-3">
+    <h1>{{ count }}</h1>
+    <button v-on:click="count++" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">increase</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
